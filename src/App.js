9a3b706bf3e46e4ffb411791fdd091334f845d92
siva adi <sivakumar.ittech@gmail.com>
2019-07-30 23:18:37 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+function App() {
+  return (
+    <div><h1>this is testing</h1><h2>this is h2</h2>
+    
+<img src="https://www.w3schools.com//w3images/forestbridge.jpg"></img>
+    </div>
+  );
+}
+
+export default App;
